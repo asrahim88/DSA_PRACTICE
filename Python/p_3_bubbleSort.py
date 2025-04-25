fd53c38ae36b2002ca32b843ee196307c9e7f420
asrahim88 <tasmimhasan1997@gmail.com>
2025-04-25 11:22:05 +0600

    bubble sort practice

--- Python/p_3_bubbleSort.py
+++ Python/p_3_bubbleSort.py
@@ -1,8 +1,10 @@
-my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+my_array = [64, 34, 25, 12, 22, 11, 5]
+n = len(my_array)
 
-for i in range(len(my_array)-1):
+
+for i in range(n-1):
     swapped = False
-    for j in range(len(my_array)-i-1):
+    for j in range(n-i-1):
         if my_array[j] > my_array[j+1]:
             my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
             swapped = True
